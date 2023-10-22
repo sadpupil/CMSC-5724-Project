c56ce833328d266e81ec9d11bdcf1c28bf039427
cyj <yjchen0819@gmail.com>
2023-10-22 12:11:05 +0800

    change the output form

--- MarginPerceptron/src/main/Main.java
+++ MarginPerceptron/src/main/Main.java
@@ -7,6 +7,7 @@ import tools.ResultVerify;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -26,26 +27,13 @@ public class Main {
             ArrayList<DataInstance> dataSet = dataReader.readData();
             double[] w = incremental.runIncremental(dataSet);
             System.out.println("** DataSet " + cnt + " **");
-            System.out.print("linear classifier: W");
-            printW(w);
+            System.out.println("linear classifier: w" + Arrays.toString(w));
             System.out.println("margin of the classifier: " + resultVerify.getMargin(dataSet, w));
             System.out.println("final gamma guess: " + incremental.getGamma());
             System.out.println("empirical error: " + resultVerify.getEmpiricalError(dataSet, w) + "%");
             System.out.println();
             cnt++;
         }
-    }
-
-    private static void printW(double[] w) {
-        int len = w.length;
-        System.out.print("(");
-        for(int i = 0; i < len; i++) {
-            System.out.print(w[i]);
-            if(i < len - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println(")");
     }
 
 }
